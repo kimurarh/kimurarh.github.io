72b1a53f25d94a52be092801ca00b921500275f4
Rafael Kimura <rafaelkimura94@gmail.com>
2020-07-09 08:14:15 -0300

    changes to collapse menu

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,24 @@ var navItems = document.getElementsByClassName("nav-item");
 var widthLimit = 600;
 var clientWidth = document.body.clientWidth;
 
-/* Collapse menu after clicking an option */
+/* toggle "collapse" class */
+function toggleClass() {
+    for (var j = 0; j < navItems.length; j++) {
+        navItems[j].classList.toggle('collapse');
+    }
+}
+
+/* Listens for click on "nav-items" */
 for (var i = 0; i < navItems.length; i++) {
     navItems[i].addEventListener("click", () => {
         /* Only collapse if on mobile */
         if(clientWidth <= widthLimit) {
-            for (var j = 0; j < navItems.length; j++) {
-                navItems[j].style.display = "none";
-            }
+            toggleClass();
         }
     })
 }
 
-/* Collapse menu */
+/* Listens for clicks on "menu" button */
 menu.addEventListener("click", () => {
-    for (var i = 0; i < navItems.length; i++) {
-        if (navItems[i].style.display === "none") {
-            navItems[i].style.display = "block";
-        } else {
-            navItems[i].style.display = "none";
-        }
-    }
+    toggleClass();
 });
